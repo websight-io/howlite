e478177dc497b33b151c8b0db38b4a1ddee3a6a8
Zofia Zmojdzin <zofia.zmojdzin@ds.pl>
2022-08-03 09:56:53 +0200

    HL-257 prepare basic snapshot tests

---
+++ tests/end-to-end/tests/image.cy.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 Dynamic Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+describe('Image component', function () {
+  it('renders correctly in preview mode', function () {
+    cy.login();
+
+    cy.visit('/content/howlite-test/pages/Image.html');
+
+    cy.percySnapshot();
+  });
+
+  it('renders correctly in edit mode', function () {
+    cy.login();
+
+    cy.visit(
+      '/apps/websight/index.html/content/howlite-test/pages/Image::editor'
+    );
+
+    cy.percySnapshot();
+
+    // TO DO: HL-257
+  });
+});
